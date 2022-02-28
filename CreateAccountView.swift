c8244b35a089670a75654bc535ee1cd71367467f
MJ <michellem2023@student.cis.edu.hk>
2022-02-28 17:36:46 +0800

    just for test <MJ>

--- CreateAccountView.swift
+++ CreateAccountView.swift
@@ -7,13 +7,16 @@
 
 import SwiftUI
 
-struct CreateAccountView: View {
+struct CreateAccountView: View
+{
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Yes")
     }
 }
 
-struct CreateAccountView_Previews: PreviewProvider {
+struct CreateAccountView_Previews: PreviewProvider
+{
     static var previews: some View {
         CreateAccountView()
     }
