a82cd4c1995de4635d484a2d7e2b6dc57d221fbf
ttai2023 <territ2023@student.cis.edu.hk>
2022-03-09 20:45:04 +0800

    T.T added constants

---
+++ Helper/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  CIS Friender
+//
+//  Created by Terri Tai on 9/3/2022.
+//
+
+import Foundation
+import SwiftUI
+
+enum Constants {
+    static let lightGrey = Color(red: 0.827, green: 0.870, blue: 0.894, opacity: 0.5)
+    static let lightBlue = Color(red: 0.619, green: 0.803, blue: 0.913)
+    static let skyBlue = Color(red: 0.313, green: 0.623, blue: 0.807)
+    static let blue = Color(red: 0.176, green: 0.533, blue: 0.741)
+    static let darkBlue = Color(red: 0, green: 0.407, blue: 0.647)
+}
