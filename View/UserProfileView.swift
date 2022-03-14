b888c636b756806304766eee43943d075202c3c8
MJ <michellem2023@student.cis.edu.hk>
2022-03-14 22:52:45 +0800

    [MJ] did the photo picker but still need to figure out how to add photo to the library

--- View/UserProfileView.swift
+++ View/UserProfileView.swift
@@ -19,13 +19,15 @@ struct UserProfileView: View {
     @State public var mbti: String = ""
     @State private var isEditing = false
     
+    //for image picker
+    @State private var image = UIImage()
+    @State private var showSheet = false
+    
     var aOrB = ["A", "B"]
     @State private var toAOrNotToB = "A"
     
     var signs = ["Unknown", "Taurus", "Cancer", "Virgo", "Capricorn"]
     var mbtis = ["Unknown", "ISTJ", "INFJ", "ENFJ", "ISTP"]
-
-    var colors = ["Red", "Green", "Blue", "Tartan"]
     @State private var selectedColor = "Red"
     
     let lightGrey = Color(red: 0.827, green: 0.870, blue: 0.894, opacity: 0.5)
@@ -36,14 +38,32 @@ struct UserProfileView: View {
         
         VStack(alignment: .center, spacing: 10.0)
         {
-            Image("Boy 1")
+            Image(uiImage: self.image)
                 .resizable() //so image can be resized
                 .aspectRatio(contentMode: .fill) //prevents original photo to be distorted
                 .frame(width: 150, height: 150)//frame of circle
                 .clipped() //area outside of frame will be cut
                 .cornerRadius(100)
                 .padding(.bottom, 10)
-            Button("Toggle Edit Mode") {
+            
+            // add a button for the user to change profile pic
+            Text("Change profile photo")
+                    .font(.headline)
+                    .frame(maxWidth:200)
+                    .frame(height: 25)
+                    .background(LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 0.262745098, green: 0.0862745098, blue: 0.8588235294, alpha: 1)), Color(#colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1))]), startPoint: .top, endPoint: .bottom))
+                    .cornerRadius(16)
+                    .foregroundColor(.white)
+                    .onTapGesture {
+                                showSheet = true
+                              }
+                    .padding(.horizontal, 20)
+                       .sheet(isPresented: $showSheet) {
+                                   // Pick an image from the photo library:
+                               ImagePicker(sourceType: .photoLibrary, selectedImage: self.$image)
+                       }
+            
+            Button("edit user info") {
                 isEditing.toggle()
             }
             
@@ -113,13 +133,6 @@ struct UserProfileView: View {
                         }.pickerStyle(.menu)
                         
                     }
-                    
-
-                    Picker("Color", selection: $selectedColor) {
-                                            ForEach(colors, id: \.self) {
-                                                Text($0)
-                                            }
-                                        }
                 }
             }
             
