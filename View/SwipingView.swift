790dacbac8e9ba80db41780b99fb803ad85c00df
Keona Kardashian <keonal2023@student.cis.edu.hk>
2022-03-15 11:40:29 +0800

    just to pull

--- View/SwipingView.swift
+++ View/SwipingView.swift
@@ -6,6 +6,8 @@
 //
 
 import SwiftUI
+import Firebase
+import FirebaseAuth
 
 struct SwipingView: View {
 //    @State var swipeDirection: SwipeDirection = .none
@@ -34,79 +36,79 @@ struct SwipingView: View {
     
     
     var body: some View {
-        VStack {
-            HStack {
-                Button (action: {}) {
-                    Image(systemName: "person.crop.square")
-                        .resizable().aspectRatio(contentMode: .fit).frame(height: 45)
-                        .foregroundColor(Constants.blue)
-                }
-                Text("Friender")
-                    .font(.system(size: 45))
-                    .foregroundColor(Constants.blue)
-            }.padding(.horizontal).frame(height: 45)
-            
-            ZStack {
-                ForEach(CISUser.data.reversed()) { user in
-                    CardView(card: user).padding(8)
-                }
-            }
-            
-            HStack {
-                Button(action: {}) {
-                    Image(systemName: "arrowshape.turn.up.backward.circle")
-                        .resizable().aspectRatio(contentMode: .fit).frame(height: 45)
-                        .foregroundColor(Constants.darkBlue)
-                }
-                Spacer()
-                Button(action: {}) {
-                    Image(systemName: "xmark.circle")
-                        .resizable().aspectRatio(contentMode: .fit).frame(height: 45)
-                        .foregroundColor(Constants.blue)
-                }
-                Spacer()
-                Button(action: {}) {
-                    Image(systemName: "info.circle")
-                        .resizable().aspectRatio(contentMode: .fit).frame(height: 45)
-                        .foregroundColor(Constants.blue)
-                }
-                Spacer()
-                Button(action: {}) {
-                    Image(systemName: "heart.circle")
-                        .resizable().aspectRatio(contentMode: .fit).frame(height: 45)
-                        .foregroundColor(Constants.blue)
-                        .foregroundColor(Constants.blue)
-                }
-                Spacer()
-                Button(action: {}) {
-                    Image(systemName: "star.circle")
-                        .resizable().aspectRatio(contentMode: .fit).frame(height: 45)
-                        .foregroundColor(Constants.darkBlue)
-                }
-                
-            }.padding(.horizontal)
-        }
-        .navigationBarHidden(true)
-    }
-    
-    func getUsers(){
-        Firestore.firestore().collection("User")
-            .getDocuments() { (querySnapshot, err) in
-                //catch error
-                if let err = err {
-                    print("Error getting users: \(err)")
-                }
-                else {
-                      for document in querySnapshot!.documents {
-                          //try? -> self.user will be nil, try! -> app will crash
-                          //convert document into CISUser object
-                          self.user = try? document.data(as: CISUser.self)
-                          //loop through each tag in tags Array in current user
-                      }
-                }
-            }
-    }
-    
+//        VStack {
+//            HStack {
+//                Button (action: {}) {
+//                    Image(systemName: "person.crop.square")
+//                        .resizable().aspectRatio(contentMode: .fit).frame(height: 45)
+//                        .foregroundColor(Constants.blue)
+//                }
+//                Text("Friender")
+//                    .font(.system(size: 45))
+//                    .foregroundColor(Constants.blue)
+//            }.padding(.horizontal).frame(height: 45)
+//            
+//            ZStack {
+//                ForEach(CISUser.data.reversed()) { user in
+//                    CardView(card: user).padding(8)
+//                }
+//            }
+//            
+//            HStack {
+//                Button(action: {}) {
+//                    Image(systemName: "arrowshape.turn.up.backward.circle")
+//                        .resizable().aspectRatio(contentMode: .fit).frame(height: 45)
+//                        .foregroundColor(Constants.darkBlue)
+//                }
+//                Spacer()
+//                Button(action: {}) {
+//                    Image(systemName: "xmark.circle")
+//                        .resizable().aspectRatio(contentMode: .fit).frame(height: 45)
+//                        .foregroundColor(Constants.blue)
+//                }
+//                Spacer()
+//                Button(action: {}) {
+//                    Image(systemName: "info.circle")
+//                        .resizable().aspectRatio(contentMode: .fit).frame(height: 45)
+//                        .foregroundColor(Constants.blue)
+//                }
+//                Spacer()
+//                Button(action: {}) {
+//                    Image(systemName: "heart.circle")
+//                        .resizable().aspectRatio(contentMode: .fit).frame(height: 45)
+//                        .foregroundColor(Constants.blue)
+//                        .foregroundColor(Constants.blue)
+//                }
+//                Spacer()
+//                Button(action: {}) {
+//                    Image(systemName: "star.circle")
+//                        .resizable().aspectRatio(contentMode: .fit).frame(height: 45)
+//                        .foregroundColor(Constants.darkBlue)
+//                }
+//                
+//            }.padding(.horizontal)
+//        }
+//        .navigationBarHidden(true)
+//    }
+//    
+//    func getUsers(){
+//        Firestore.firestore().collection("User")
+//            .getDocuments() { (querySnapshot, err) in
+//                //catch error
+//                if let err = err {
+//                    print("Error getting users: \(err)")
+//                }
+//                else {
+//                      for document in querySnapshot!.documents {
+//                          //try? -> self.user will be nil, try! -> app will crash
+//                          //convert document into CISUser object
+//                          self.user = try? document.data(as: CISUser.self)
+//                          //loop through each tag in tags Array in current user
+//                      }
+//                }
+//            }
+//    }
+//    
 }
 
 struct SwipingView_Previews: PreviewProvider {
