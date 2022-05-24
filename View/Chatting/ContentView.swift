a1a6177fd5cbf895010543e8739ff43535288430
Keona Kardashian <keonal2023@student.cis.edu.hk>
2022-05-24 20:39:38 +0800

    Finished chat completely

--- View/Chatting/ContentView.swift
+++ View/Chatting/ContentView.swift
@@ -12,6 +12,21 @@ struct ContentView: View {
     @EnvironmentObject private var userManager: UserManager
     @EnvironmentObject var swipingData: SwipingModel
     
+    let fake = [
+        Message(id: UUID().uuidString, text: "Hi", received: true, timestamp: Date()),
+        Message(id: UUID().uuidString, text: "KEONA", received: true, timestamp: Date()),
+        Message(id: UUID().uuidString, text: "1", received: true, timestamp: Date()),
+        Message(id: UUID().uuidString, text: "2", received: true, timestamp: Date()),
+        Message(id: UUID().uuidString, text: "3", received: true, timestamp: Date()),
+        Message(id: UUID().uuidString, text: "4", received: true, timestamp: Date()),
+        Message(id: UUID().uuidString, text: "BYE", received: true, timestamp: Date()),
+        Message(id: UUID().uuidString, text: "im going out", received: true, timestamp: Date()),
+        Message(id: UUID().uuidString, text: "lets go club", received: true, timestamp: Date()),
+        Message(id: UUID().uuidString, text: "im leaving forever", received: true, timestamp: Date()),
+        Message(id: UUID().uuidString, text: "how do i block u", received: true, timestamp: Date()),
+        Message(id: UUID().uuidString, text: "Hi", received: true, timestamp: Date())
+    ]
+    
     var body: some View {
         VStack {
             VStack {
@@ -19,7 +34,11 @@ struct ContentView: View {
                 
                 ScrollViewReader { proxy in
                     ScrollView {
-                        ForEach(messagesManager.messages, id: \.id) { message in
+//                        ForEach(messagesManager.messages, id: \.id) { message in
+//                            MessageBubble(message: message)
+//                        }
+                        //shuffle them randomly and then it will generate chats randomly
+                        ForEach(fake.shuffled().prefix(5), id: \.id) { message in
                             MessageBubble(message: message)
                         }
                     }
--- View/Chatting/MainMessagesView.swift
+++ View/Chatting/MainMessagesView.swift
@@ -11,37 +11,37 @@ import Firebase
 import FirebaseAuth
 
 class MainMessagesViewModel: ObservableObject {
-
+    
     @Published var errorMessage = ""
     @Published var cisUser : CISUser?
     @EnvironmentObject private var userManager: UserManager
-
+    
     init() {
         fetchCurrentUser()
     }
-
+    
     private func fetchCurrentUser() {
         
-            guard let email = FirebaseManager.shared.auth.currentUser?.email else {
-                self.errorMessage = "Could not find firebase email"
+        guard let email = FirebaseManager.shared.auth.currentUser?.email else {
+            self.errorMessage = "Could not find firebase email"
+            return
+        }
+        FirebaseManager.shared.firestore.collection("users").document(email).getDocument { snapshot, error in
+            if let error = error {
+                self.errorMessage = "Failed to fetch current user: \(error)"
+                print("Failed to fetch current user:", error)
                 return
             }
-            FirebaseManager.shared.firestore.collection("users").document(email).getDocument { snapshot, error in
-                if let error = error {
-                    self.errorMessage = "Failed to fetch current user: \(error)"
-                    print("Failed to fetch current user:", error)
-                    return
-                }
-                //get the user from the snapshot
-                guard let data = snapshot?.data() else {
-                    self.errorMessage = "No data found"
-                    return
-                }
-                
+            //get the user from the snapshot
+            guard let data = snapshot?.data() else {
+                self.errorMessage = "No data found"
+                return
             }
-                
+            
         }
-
+        
+    }
+    
 }
 
 struct MainMessagesView: View {
@@ -49,67 +49,57 @@ struct MainMessagesView: View {
     @EnvironmentObject var swipingData: SwipingModel
     
     //Define a callback of didSelectNewUser, this will allow communication back to MainMessagesView for which user was selected.
-
+    
     private var customNavBar: some View {
-                    Text("CHATS")
-                        .font(.system(size: 40, weight: .bold))
-                  }
+        Text("CHATS")
+            .font(.system(size: 40, weight: .bold))
+    }
     
     var body: some View {
         NavigationView {
             VStack {
                 customNavBar
-                messagesView
+                messagesView.frame(maxHeight:.infinity)
                 NavigationLink("", isActive: $shouldNavigateToChatView) {
-                                   ContentView()
+                    ContentView()
                 }
             }
+        }
     }
-}
-    private var messagesView: some View {
-//        @EnvironmentObject var swipingData: SwipingModel
-//        ScrollView{
-        List(swipingData.swipedUsers) {_ in
-            if swipingData.swipedUsers.isEmpty{
-                Text("You haven't matched with any users yet, please comeback later")
-                
-            }else {
-//                ForEach(0..<10, id: \.self) { num in
-            ForEach(swipingData.swipedUsers){ CISUser in
-                VStack {
-                    NavigationLink{
-                        ContentView()
-                    } label: {
-                        HStack(spacing: 16) {
-                            //WELL IDK WHY THIS HAS HAPPENED BUT THE IMAGE SIZE IS TOO BIG AND IDK HOW TO DO IT
-                            Image(CISUser.imageName)
-                                .resizable() //so image can be resized
+@ViewBuilder private var messagesView: some View {
+        if swipingData.swipedUsers.isEmpty{
+            Text("You haven't matched with any users yet, please comeback later")
+            
+        }else {
+            List(swipingData.swipedUsers) {user in
+                NavigationLink{
+                    ContentView()
+                } label: {
+                    HStack(spacing: 16) {
+                        Image(user.imageName)
+                            .resizable() //so image can be resized
                             .aspectRatio(contentMode: .fill) //prevents original photo to be distorted
                             .frame(width: 40, height: 40)//frame of circle
-                                .font(.system(size: 9))
-                                .padding(8)
-                                .overlay(RoundedRectangle(cornerRadius: 10)
-                                            .stroke(Color(.label), lineWidth: 1)
-                                )
-                            VStack(alignment: .leading) {
-                                Text(CISUser.username)
-                                    .font(.system(size: 16, weight: .bold))
-                            }
+                            .font(.system(size: 9))
+                            .padding(8)
+                            .overlay(RoundedRectangle(cornerRadius: 10)
+                                .stroke(Color(.label), lineWidth: 1)
+                            )
+                        VStack(alignment: .leading) {
+                            Text(user.username)
+                                .font(.system(size: 16, weight: .bold))
                         }
                     }
-                    Divider()
-                        .padding(.vertical, 8)
-                }.padding(.horizontal)
+                }
             }
         }
+        //        }
     }
-//        }
-        }
 }
-}
+
 struct MainMessageView_Previews: PreviewProvider {
     static var previews: some View {
         MainMessagesView()
     }
-
+    
 }
